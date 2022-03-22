0d80ee866672312e0b3cc0c32432f3d8e399c456
jaydainn <37037667+jaydainn@users.noreply.github.com>
2022-03-22 14:07:02 +0100

    ✨ history

--- src/pages/History.js
+++ src/pages/History.js
@@ -1,16 +1,47 @@
 import React from "react"
-import {Heading , Table} from "evergreen-ui"
+import {Heading , Table , StatusIndicator} from "evergreen-ui"
 
 const History = () => {
 
     return (
         <>
-        
+       <Table.Body> 
   <Table.Head>
     <Table.SearchHeaderCell />
     <Table.TextHeaderCell>Date</Table.TextHeaderCell>
     <Table.TextHeaderCell>Workout</Table.TextHeaderCell>
   </Table.Head>
+  <Table.Body>
+    <Table.Row>
+      <Table.TextCell flexBasis={560} flexShrink={0} flexGrow={0}>
+      <StatusIndicator color="success">Done</StatusIndicator>
+      </Table.TextCell>
+      <Table.TextCell>{new Date().toLocaleDateString("en-US")}</Table.TextCell>
+      <Table.TextCell>Lat pull down</Table.TextCell>
+    </Table.Row>
+    <Table.Row>
+      <Table.TextCell flexBasis={560} flexShrink={0} flexGrow={0}>
+      <StatusIndicator color="success">Done</StatusIndicator>
+      </Table.TextCell>
+      <Table.TextCell>{new Date().toLocaleDateString("en-US")}</Table.TextCell>
+      <Table.TextCell>Lat pull down</Table.TextCell>
+    </Table.Row>
+    <Table.Row>
+      <Table.TextCell flexBasis={560} flexShrink={0} flexGrow={0}>
+      <StatusIndicator color="success">Done</StatusIndicator>
+      </Table.TextCell>
+      <Table.TextCell>{new Date().toLocaleDateString("en-US")}</Table.TextCell>
+      <Table.TextCell>Lat pull down</Table.TextCell>
+    </Table.Row>
+    <Table.Row>
+      <Table.TextCell flexBasis={560} flexShrink={0} flexGrow={0}>
+      <StatusIndicator color="success">Done</StatusIndicator>
+      </Table.TextCell>
+      <Table.TextCell>{new Date().toLocaleDateString("en-US")}</Table.TextCell>
+      <Table.TextCell>Lat pull down</Table.TextCell>
+    </Table.Row>
+  </Table.Body>
+</Table.Body>
   
 
         </>
